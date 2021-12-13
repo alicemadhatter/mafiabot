2f95bf29216a5edc909c1ea790bf9b323ee7711c
Ildar <wilidon@bk.ru>
2021-12-13 17:01:13 +0400

    fixed bugs

--- mafia.py
+++ mafia.py
@@ -8,14 +8,18 @@ import time
 async def main(name, password, rucaptcha_api):
     mpets = MpetsApi(name=name, password=password, rucaptcha_api=rucaptcha_api, timeout=5, fast_mode=True)
     resp = await mpets.login()
+    if resp.status is False:
+        print("Не удалось авторизоваться.")
     while True:
-        await mpets.actions(3)
-        time.sleep(7200)
+        time.sleep(3)
         mailresp = await mpets.view_posters()
+        if mailresp.status is False:
+            continue
         for mail in mailresp.players:
-            print(mail)
             if mail.unread == True:
                 mailcheck = await mpets.post_message(mail.pet_id)
+                if mailcheck['status'] is False:
+                    continue
                 print("mail", mailcheck)
                 if "Старт" in mailcheck['messages'][0]['text']:
                     mailsend = await mpets.post_send("Добро пожаловать! Я помогаю проводить Мафию в режиме "
@@ -26,19 +30,27 @@ async def main(name, password, rucaptcha_api):
                                                      "В начале напишите \"СПИСОК РОЛЕЙ\", так я пойму, что Вы добавляете"
                                                      "роли в игру!" + ":-)" * random.randint(1, 10), mail.pet_id)
                 if "СПИСОК РОЛЕЙ" in mailcheck['messages'][0]['text']:
-                    temp = mailcheck['messages'][0]['text'].split("\n")[1:]
+                    temp = mailcheck['messages'][0]['text']
                     temp = temp.split("\n")[1:]
                     text = "Обнаружены следующие роли:\n"
+                    counter = 1
+                    tmp = []
                     for i in temp:
-                        text += '-'.join([*i.split(". ")[1].split(":")])
-                        text += '\n'
-                    mailsend = await mpets.post_send(text)
+                        n_c = i.split(". ")[1].split(":")
+                        for i in range(0, int(n_c[1])):
+                            tmp.append([n_c[0]])
+                    random.shuffle(tmp)
+                    for i in tmp:
+                        text += f"{counter}. {i[0]} \n"
+                        counter += 1
+                    text += "  " * random.randint(1,100)
+                    mailsend = await mpets.post_send(text, pet_id=mail.pet_id)
 
 
 if __name__ == '__main__':
-    name = "ВВЕДИТЕ СЮДА ЛОГИН ПИТОМЦА"
-    password = "ВВЕДИТЕ СЮДА ПАРОЛЬ ПИТОМЦА"
-    rucaptcha_api = "ВВЕДИТЕ СЮДА ТОКЕН ДОСТУПА RUCAPTCHA (если есть)"
+    name = " "
+    password = " "
+    rucaptcha_api = " "
     asyncio.run(main(name=name,
                      password=password,
                      rucaptcha_api=rucaptcha_api))
