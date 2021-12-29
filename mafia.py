fda2ef8d300a8a5be4e0f9383c3759429eaae5a5
Gleb <96059418+alicemadhatter@users.noreply.github.com>
2021-12-29 22:20:58 +0300

    fixed critical bug (provided by Wilidon)

--- mafia.py
+++ mafia.py
@@ -4,7 +4,6 @@ from mpets import MpetsApi
 import random
 import time
 
-
 async def main(name, password, rucaptcha_api):
     mpets = MpetsApi(name=name, password=password, rucaptcha_api=rucaptcha_api, timeout=5, fast_mode=True)
     resp = await mpets.login()
@@ -24,12 +23,24 @@ async def main(name, password, rucaptcha_api):
                 if "Старт" in mailcheck['messages'][0]['text']:
                     mailsend = await mpets.post_send("Добро пожаловать! Я помогаю проводить Мафию в режиме "
                                                      "«как в разрушителях». Отправьте слово «ИГРА», "
-                                                     "чтобы начать (обязательно КАПСОМ) " + ":]" * random.randint(1,10), mail.pet_id)
+                                                     "чтобы начать (обязательно КАПСОМ, я иначе не пойму) " + ":]" * random.randint(1,10), mail.pet_id)
                 if "ИГРА" in mailcheck['messages'][0]['text']:
                     mailsend = await mpets.post_send("Напишите названия ролей и их количество в игре. "
                                                      "Формат: название роли:количество человек на роли. "
                                                      "Укажите в начале «СПИСОК РОЛЕЙ», чтобы я понял, что Вы хотите добавить "
-                                                     "роли!" + ":-)" * random.randint(1, 10), mail.pet_id)
+                                                     "роли!"
+                                                     "\n"
+                                                     "\n"
+                                                     "Пример:\n"
+                                                     "СПИСОК РОЛЕЙ\n"
+                                                     "1. Мирный житель:4\n"
+                                                     "2. Мафия:3\n"
+                                                     "3. Комиссар:1\n"
+                                                     "4. Доктор:1\n"
+                                                     "5. Горожанка:1\n"
+                                                     "\n"
+                                                     "\n"
+                                                     "Жду Вашего ответа с нетерпением" + ":-)" * random.randint(1, 10), mail.pet_id)
                 if "СПИСОК РОЛЕЙ" in mailcheck['messages'][0]['text']:
                     temp = mailcheck['messages'][0]['text']
                     temp = temp.split("\n")[1:]
@@ -37,9 +48,12 @@ async def main(name, password, rucaptcha_api):
                     counter = 1
                     tmp = []
                     for i in temp:
-                        n_c = i.split(". ")[1].split(":")
-                        for i in range(0, int(n_c[1])):
-                            tmp.append([n_c[0]])
+                            try:
+                                n_c = i.split(". ")[1].split(":")
+                                for i in range(0, int(n_c[1])):
+                                    tmp.append([n_c[0]])
+                            except:
+                                pass
                     random.shuffle(tmp)
                     for i in tmp:
                         text += f"{counter}. {i[0]} \n"
@@ -49,9 +63,9 @@ async def main(name, password, rucaptcha_api):
 
 
 if __name__ == '__main__':
-    name = " "
-    password = " "
-    rucaptcha_api = " "
+    name = "PET NAME"
+    password = "PET PASSWORD"
+    rucaptcha_api = "API RUCAPTCHA"
     asyncio.run(main(name=name,
                      password=password,
                      rucaptcha_api=rucaptcha_api))
